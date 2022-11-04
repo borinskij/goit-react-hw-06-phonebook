27421e2459fcb16a05e805e29085d3fe3f8a04f1
Abdrii Borinskij <aborinskij@gmail.com>
2022-11-04 02:28:53 +0200

    base url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider store={store} basename="goit-react-hw-06-phonebook">
     <React.StrictMode>
       <App />
     </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@ import { createStore } from 'redux';
 import { phonebookInitialState } from './Phonebook/initial-state.phonebook';
 import { phonebookReducer } from './Phonebook/reduser.phonebook';
 
-
 const enhancer = devToolsEnhancer();
 export const store = createStore(
   phonebookReducer,
